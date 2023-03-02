c5611a60a14f4da73d352218d66f85783b7f0364
daxx0ne <jcn07253@gmail.com>
2023-03-02 10:13:14 +0900

    2단계

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -11,11 +11,17 @@ public class Application {
     public void run() {
         System.out.println("== 명언 앱 ==");
         while(true){
-            System.out.println("명령) ");
+            System.out.print("명령) ");
             String command = sc.nextLine().trim();
             // trim() : 혹시 있을지 모를 좌우공백제거된 버전으로 주세요!
-            if(command.equals("종료")) {
+            if(command.equals("종료")) { // 종료가 입력되면 프로그램 종료
                 break;
+            }
+            else if (command.equals("등록")){
+                System.out.print("명언 : "); // 명언과 작가 이름을 입력받기
+                String content = sc.nextLine().trim();
+                System.out.print("작가 : ");
+                String authorName = sc.nextLine().trim();
             }
 
         }
--- src/main/java/com/example/main.java
+++ src/main/java/com/example/main.java
@@ -1,10 +1,10 @@
 package com.example;
 import java.util.*;
-public class main {
+public class main { // Applicaiton이 일하도록 함
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        new Application(sc).run();
+        new Application(sc).run(); // 기본적으로 Scanner를 가지고 있는 Applicaiton
         sc.close();
     }
 }
