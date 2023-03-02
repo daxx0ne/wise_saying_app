c082665ccde36c4e4efcb1ee45d2b51ecc250ac9
daxx0ne <jcn07253@gmail.com>
2023-03-02 10:28:29 +0900

    4단계

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -10,18 +10,24 @@ public class Application {
 
     public void run() {
         System.out.println("== 명언 앱 ==");
+
+        long lastWiseSayingId = 1; // 제일 겉에 있는 곳에 변수를 선언해야지 장기기억!
+
         while(true){
             System.out.print("명령) ");
             String command = sc.nextLine().trim();
-            // trim() : 혹시 있을지 모를 좌우공백제거된 버전으로 주세요!
+            // trim() : 혹시 있을지 모를 좌우공백 제거된 버전으로 주세요!
             if(command.equals("종료")) { // 종료가 입력되면 프로그램 종료
                 break;
             }
             else if (command.equals("등록")){
-                System.out.print("명언 : "); // 명언과 작가 이름을 입력받기
+                System.out.print("명언 : "); // #2 명언과 작가 이름을 입력받기
                 String content = sc.nextLine().trim();
                 System.out.print("작가 : ");
                 String authorName = sc.nextLine().trim();
+
+                System.out.printf("%d번 명언이 등록되었습니다.\n",lastWiseSayingId); // #3 등록 문구 띄우기
+                lastWiseSayingId++; // #4 등록할 때마다 번호 증가
             }
 
         }
---
+++ src/main/java/com/example/WiseSaying.java
@@ -0,0 +1,6 @@
+package com.example;
+
+public class WiseSaying {
+    private long id;
+    //private string
+}
